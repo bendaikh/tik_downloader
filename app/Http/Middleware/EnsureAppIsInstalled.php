1a6d93eae73437fed3b42e496e5528930ee3b270
bendaikh <bendaikh2000@gmail.com>
2025-08-24 04:29:25 -0700

    commited

--- app/Http/Middleware/EnsureAppIsInstalled.php
+++ app/Http/Middleware/EnsureAppIsInstalled.php
@@ -4,9 +4,11 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Contracts\Container\BindingResolutionException;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class EnsureAppIsInstalled
 {
@@ -14,11 +16,11 @@ class EnsureAppIsInstalled
      * Handle an incoming request.
      *
      * @param Request $request
-     * @param Closure(Request): (Response|RedirectResponse) $next
-     * @return Response|RedirectResponse
+     * @param Closure(Request): (Response|RedirectResponse|JsonResponse|StreamedResponse) $next
+     * @return Response|RedirectResponse|JsonResponse|StreamedResponse
      * @throws BindingResolutionException
      */
-    public function handle(Request $request, Closure $next): Response|RedirectResponse
+    public function handle(Request $request, Closure $next): Response|RedirectResponse|JsonResponse|StreamedResponse
     {
         // Skip installer routes to prevent redirect loop
         if ($request->is('install*')) {
