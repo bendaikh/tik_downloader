0d73237f841c029e9c781161df80bb74999007a8
bendaikh <bendaikh2000@gmail.com>
2025-09-09 04:08:32 -0700

    commited

--- resources/views/components/admin/navigation.blade.php
+++ resources/views/components/admin/navigation.blade.php
@@ -26,7 +26,7 @@
     <x-admin.nav-link
         href="{{route('admin.test')}}"
         icon="admin.icon.mini.test"
-        text="Test"
+        text="Test - UPDATED VIA 1.1.9"
     />
     
     <x-admin.nav-link
